75fd313fa0b07040e157f72670c2604c521ae141
Arthur-Nevess <arthurneves0a12@gmail.com>
2024-05-22 16:39:14 -0300

    Cadastrando dados do usuario no banco de dados

--- Regra_Negocio.c
+++ Regra_Negocio.c
@@ -2,27 +2,13 @@
 #include <stdlib.h>
 #include <string.h>
 #include "Regra_Negocio.h"
-#define TAM 50
 
+informações inf;
+inde ind;
 
-void separa(char dados[100])
+void separa(char dados[120])
 {
-  //Variaveis de transposição  
-
-    char nome[TAM]={0};
-    char cpf[TAM]={0};
-    char conta[TAM]={0};
-    char senha[TAM]={0};
-    char tipo[TAM]={0};
-
-  //Variaveis de acesso de índice  
-
-    int nome_a=0;
-    int cpf_a=0;
-    int conta_a=0;
-    int senha_a=0;
-    int tipo_a=0;
-    
+ 
   //Variaveis de verificação  
 
     int freio=0;
@@ -37,29 +23,29 @@ void separa(char dados[100])
             
             if(freio==0)
             {
-                nome[nome_a++]=dados[i];
+                inf.nome[ind.nome_a++]=dados[i];
             }
 
             else if(freio==1)
             {
-                conta[conta_a++] = dados[i];
+                inf.conta[ind.conta_a++] = dados[i];
             }
 
             else if(freio==2)
             {            
-                cpf[cpf_a++]=dados[i];
+                inf.cpf[ind.cpf_a++]=dados[i];
             }
 
             else if(freio==3)
             {
                 
-                tipo[tipo_a++]=dados[i];
+                inf.tipo[ind.tipo_a++]=dados[i];
             }
 
             else if(freio==4)
             {
                 
-                senha[senha_a++]=dados[i];
+                inf.senha[ind.senha_a++]=dados[i];
             }
         }
 
@@ -71,19 +57,19 @@ void separa(char dados[100])
 
   //Pondo caracter núlo  
 
-    nome[nome_a] = '\0';
-    conta[conta_a] = '\0';
-    cpf[cpf_a] = '\0';
-    tipo[tipo_a] = '\0';
-    senha[senha_a] = '\0';
+    inf.nome[ind.nome_a] = '\0';
+    inf.conta[ind.conta_a] = '\0';
+    inf.cpf[ind.cpf_a] = '\0';
+    inf.tipo[ind.tipo_a] = '\0';
+    inf.senha[ind.senha_a] = '\0';
 
   //Impressão dos dados separadamente
 
-    printf("%s\n", nome);
-    printf("%s\n", cpf);
-    printf("%s\n", conta);
-    printf("%s\n", tipo); 
-    printf("%s\n", senha);
+    printf("%s\n", inf.nome);
+    printf("%s\n", inf.cpf);
+    printf("%s\n", inf.conta);
+    printf("%s\n", inf.tipo); 
+    printf("%s\n", inf.senha);
   
 
 }
@@ -93,23 +79,85 @@ void contaExiste()
 
   //Abertura e verificação do arquivo
     FILE *f;
-    char contaFull[100];
+    char contaFull[120];
     f = fopen("Accounts.txt", "r");
-    if(f != NULL){
-        fscanf(f, " %s" ,contaFull);
-    }
-    else
+    if(f == NULL)
     {
-        putss("Não foi possivel abrir o banco de dados");
+        puts("Não foi possivel abrir o banco de dados");
     }
-   
+    int  n=0;
+
+    fscanf(f,"%d",&n);
+    for(int i=0;i<n;i++);
+    {
+        fscanf(f, " %s" ,&contaFull);
+    }
+    printf("(%s)\n",contaFull);
     separa(contaFull);
     
     fclose(f);
 }
 
+void cadastro()
+{
+  //Espaço variaveis  
+    char po[11];
+    char co[10];
+    sprintf(po,"poupança");
+    sprintf(co,"corrente");
+    int c = 0;
+  //Criar validação de tamanho
+
+    puts("Digite seu nome:");
+    scanf(" %s", &inf.nome);
+
+    puts("Digite o número da sua conta");
+    scanf(" %s", &inf.conta);
+
+    puts("Digite seu cpf");
+    scanf(" %s", &inf.cpf);
+
+    puts("Para criar conta pupança tecle(1) Para conta corrente tecle(2)");
+    scanf("%d", &c);
+    switch(c)
+    {
+        case 1:
+        {
+            sprintf(inf.tipo,"poupança");
+            break;
+        }
+
+        case 2:
+        {
+             sprintf(inf.tipo, "corrente");
+             break;
+        }
+        
+    }
+
+    puts("Digite sua senha:");
+    scanf("%s", inf.senha);
+
+  //Adicionando o cadastro no banco de dados
+
+     FILE *f=fopen("Accounts.txt", "r++");
+    if(f==NULL)
+    {
+        puts("Não foi possivel acessar arquivo");
+    }
+
+    int n=0;
+    fscanf(f,"%d",&n);
+    n++;
+    fseek(f,0,SEEK_SET);
+    fprintf(f,"%d",n);
+    fseek(f,0,SEEK_END);
+    fprintf(f,"\nNome:%s,Conta:%s,CPF:%s,Tipo:%s,Senha:%s", inf.nome,inf.conta,inf.cpf,inf.tipo,inf.senha);
+}
+
 int main()
 {
+    cadastro();
     contaExiste();
     
 }
--- Regra_Negocio.h
+++ Regra_Negocio.h
@@ -1,1 +1,28 @@
+#define TAM 50
+struct tipos_dados      
+{
+    char nome [TAM];
+    char cpf  [TAM];
+    char conta[TAM];
+    char senha[TAM];
+    char tipo [TAM];
+
+};
+typedef struct tipos_dados informações;
+
+struct index
+{
+    int nome_a;
+    int cpf_a;
+    int conta_a;
+    int senha_a;
+    int tipo_a;
+    
+};
+typedef struct index inde;
+
+
 void contaExiste();
+void separa(char dados[100]);
+
+
