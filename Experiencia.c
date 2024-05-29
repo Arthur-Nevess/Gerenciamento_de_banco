1e2dcb63d7545d62891156b5883fd3c20bae325d
cleber_git <cleber.neves@newpost.com>
2024-05-28 21:18:31 -0300

    commitando para fazer um pull

--- Experiencia.c
+++ Experiencia.c
@@ -1,8 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "Regra_Negocio.h"
 
-int main()
-{
-    printf("Bank...");
-    return 0;
+void menu(){
+
+    int opt;
+    puts("||============================================||");
+    puts("||        Digite 0 para [ Sair ]              ||");
+    puts("||        Digite 1 para [ Cadastrar Conta ]   ||");
+    puts("||        Digite 2 para [ Fazer Login ]       ||");
+    puts("||        Digite 3 para [ Saber mais ]        ||");
+    puts("||============================================||");
+    
+    scanf("%d", &opt);
+    
+    switch (opt)
+    {
+    case 0:
+        exit(0);
+        break;
+    case 1:
+        
+        break;
+    default:
+        break;
+    }
+
 }
